97ac817a326d812b10edb3dbf5f7d0d6c0218962
Henryk Ratajczak <henryk.ratajczak@allegro.com>
2026-04-20 16:09:03 +0200

    at peace and wazka endings

---
+++ texts/Remain-in-czarna-wazka-ending.js
@@ -0,0 +1,4 @@
+window.TEXTS = window.TEXTS || {};
+window.TEXTS['remain-in-czarna-wazka'] = `Ktoś musiał pozostać w Ważce.
+
+Kto inny nadawał się do tego tak dobrze, jak Zygmunt?`;
---
+++ texts/at-peace-with-himself-ending.js
@@ -0,0 +1,4 @@
+window.TEXTS = window.TEXTS || {};
+window.TEXTS['at-peace-with-himself'] = `Zygmunt pogodził się sam ze sobą.
+
+Żył długo i szczęśliwie w swoim towarzystwie.`;
